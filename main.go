ba71f1667ccec7bee729e27fe857807ceb9420ba
Peter Fejes <fejes.peter@gmail.com>
2022-01-16 11:38:54 +0100

    create deck

--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
-import "fmt"
 
 func main() {
 
 	// slice
-	cards := []string{"a", "b", newCard()}
-	cards = append(cards, "new element")
+	cards := newDeck()
+	
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 
-	fmt.Println(cards)
-
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
